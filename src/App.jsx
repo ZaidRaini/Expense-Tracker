b13a20ba2f62f6f8cfe3c2f30cb5fce2ad04d640
Mohammed Zaid <128317781+ZaidRaini@users.noreply.github.com>
2024-10-03 09:19:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,59 @@
-import AddExpenseForm from "./components/AddExpenseForm";
-import Wallet from "./components/AddIncomeForm";
-import ExpenseList from "./components/ExpenseList";
-import ExpenseSummary from "./components/ExpenseSummary";
-import ExpenseTrends from "./components/ExpenseTrends";
-import { useLocalStorage } from "./hooks/useLocalStorage";
+import { useEffect, useRef, useState } from 'react'
+//styles
+import './App.css';
+//components
+import Navbar from './components/Navbar/Navbar'
+import AppHead from './components/AppHead/AppHead'
+import AppBody from './components/AppBody/AppBody';
+//contexts
+import { TransactionsContext, MoneyContext } from "./Contexts/AllContexts"
+//variables
+import { dummyData } from './dummyTransactions';
 
-const App = () => {
-  const [balance, setBalance] = useLocalStorage("walletBalance", 5000);
-  const [expenses, setExpenses] = useLocalStorage("expenses", []);
+function App() {
+  const [money, setMoney] = useState({
+    balance: 3800,
+    expenses: 1200
+  })
+  const [transactionData, setTransactionData] = useState(dummyData);
+  const initialRender = useRef(true);
+
+  useEffect(()=>{
+    if(initialRender.current)  onLoad();
+
+    return(() => {
+      initialRender.current = false;
+    })
+  }, [])
+
+  useEffect(()=> {
+    //save data to local storage and if it is initial render skip saving
+    if(!initialRender.current) localStorage.setItem("allData", JSON.stringify({money, transactionData}));
+  }, [money, transactionData])
+
+  //functions
+  const onLoad = () => {
+    //load data from local storage if present
+    const localData = localStorage.getItem("allData");
+    if(localData){
+      const {money, transactionData} = JSON.parse(localData);
+      setMoney(money);
+      setTransactionData(transactionData);
+    }
+  }
+  
 
   return (
-    <div>
-      <Wallet balance={balance} setBalance={setBalance} />
-      <AddExpenseForm
-        balance={balance}
-        setBalance={setBalance}
-        expenses={expenses}
-        setExpenses={setExpenses}
-      />
-      <ExpenseList expenses={expenses} setExpenses={setExpenses} />
-      <ExpenseSummary expenses={expenses} />
-      <ExpenseTrends expenses={expenses} />
-    </div>
-  );
-};
+    <main className='App'>
+      <MoneyContext.Provider value={[money, setMoney]}>
+      <TransactionsContext.Provider value={[transactionData, setTransactionData]}>
+        <Navbar />
+        <AppHead balance={money.balance} expenses={money.expenses}/>
+        <AppBody transactionData={transactionData}/>
+      </TransactionsContext.Provider> 
+      </MoneyContext.Provider>
+    </main>
+  )
+}
 
-export default App;
+export default App
